85f5e0fa08ec635622a0839519a97a3fe2e197b8
Johnathan Croom <johnathancroom@gmail.com>
2012-07-02 22:35:12 -0700

    Started Freemail class + tests
    Also included default email sent from iMail

---
+++ freemail.rb
@@ -0,0 +1,9 @@
+class Freemail
+  def initialize(raw_email)
+    @freemail = raw_email
+  end
+  
+  def sender
+    
+  end
+end
---
+++ tests/freemail_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../freemail'
+
+describe Freemail do
+  # Create instance before all tests
+  before :each do
+    @freemail = Freemail.new File.read('tests/emails/iMail.txt')
+  end
+  
+  describe "#new" do
+    it "takes one parameter and returns a Freemail object" do
+      @freemail.should be_an_instance_of Freemail
+    end
+  end
+  
+  describe "#sender" do
+    it "returns sender" do
+      @freemail.sender.should eql "johnathancroom@gmail.com"
+    end
+  end
+end
