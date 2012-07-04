de6d9d8c7aef3a7a9ae9a5d78224240262918999
Johnathan Croom <johnathancroom@gmail.com>
2012-07-04 11:09:58 -0700

    Added header method + passed sender test

--- freemail.rb
+++ freemail.rb
@@ -1,9 +1,17 @@
 class Freemail
   def initialize(raw_email)
-    @freemail = raw_email
+    @freemail = raw_email.split('--') # Split up headers and bodies
   end
   
-  def sender
-    
+  def header(property)
+    @freemail[0].each_line do |line|
+      line = line.split(':')
+      
+      if line[0].downcase == property
+        @value = line[1].strip
+      end
+    end
+        
+    @value
   end
 end
--- tests/freemail_spec.rb
+++ tests/freemail_spec.rb
@@ -6,15 +6,15 @@ describe Freemail do
     @freemail = Freemail.new File.read('tests/emails/iMail.txt')
   end
   
-  describe "#new" do
-    it "takes one parameter and returns a Freemail object" do
+  describe '#new' do
+    it 'takes one parameter and returns a Freemail object' do
       @freemail.should be_an_instance_of Freemail
     end
   end
   
-  describe "#sender" do
-    it "returns sender" do
-      @freemail.sender.should eql "johnathancroom@gmail.com"
+  describe '#sender' do
+    it 'returns sender' do
+      @freemail.header('from').should eql 'Dr. Nobody <nobody@gmail.com>'
     end
   end
 end
