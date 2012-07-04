a9139bd062487382ed734ae2dafd540e32fa5ce7
Johnathan Croom <johnathancroom@gmail.com>
2012-07-04 11:18:37 -0700

    Commented header method + Added header tests

--- freemail.rb
+++ freemail.rb
@@ -1,17 +1,23 @@
 class Freemail
   def initialize(raw_email)
-    @freemail = raw_email.split('--') # Split up headers and bodies
+    # Split up headers and bodies
+    @freemail = raw_email.split('--')
   end
   
   def header(property)
+    # Process through each line in the header
     @freemail[0].each_line do |line|
+      # Split the line by a colon
       line = line.split(':')
       
+      # Check if the left of the colon is the property we want
       if line[0].downcase == property
+        # Return the value of this property stripped
         @value = line[1].strip
       end
     end
-        
+    
+    # Return the value we retrieved
     @value
   end
 end
--- tests/freemail_spec.rb
+++ tests/freemail_spec.rb
@@ -6,15 +6,35 @@ describe Freemail do
     @freemail = Freemail.new File.read('tests/emails/iMail.txt')
   end
   
+  # Test Initialize
   describe '#new' do
     it 'takes one parameter and returns a Freemail object' do
       @freemail.should be_an_instance_of Freemail
     end
   end
   
-  describe '#sender' do
+  # Test headers
+  describe "#header('from')" do
     it 'returns sender' do
       @freemail.header('from').should eql 'Dr. Nobody <nobody@gmail.com>'
+    end
+  end
+  
+  describe "#header('subject')" do
+    it 'returns subject' do
+      @freemail.header('subject').should eql 'This is my subject, yo'
+    end
+  end
+  
+  describe "#header('to')" do
+    it 'returns recipient' do
+      @freemail.header('to').should eql 'Johnathan Croom <johnathancroom@gmail.com>'
+    end
+  end
+  
+  describe "#header('nonexistent_property')" do
+    it 'returns nil' do
+      @freemail.header('nonexistent_property').should eql nil
     end
   end
 end
