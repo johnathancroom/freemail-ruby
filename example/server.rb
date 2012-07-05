d27f56cac2203ec64b53733bd5db0782ce28fa3c
Johnathan Croom <johnathancroom@gmail.com>
2012-07-05 10:37:16 -0700

    Comment example

--- example/server.rb
+++ example/server.rb
@@ -1,10 +1,14 @@
-require 'sinatra'
-require 'cgi'
-require_relative '../freemail'
+require 'sinatra' # Server of choice
+require 'cgi' # Needed to escape HTML in the view
+require_relative '../freemail' # Include the Freemail class
 
+# Process HTML GET requests at the root URL
 get '/' do
+  # Read the contents of our example email
   @original = File.read(File.join(File.dirname(__FILE__), 'email.txt'))
+  # Use Freemail to parse the contents (returns hash of contents)
   @freemail = Freemail.parse @original
   
+  # Render views/example.erb
   erb :example
 end
